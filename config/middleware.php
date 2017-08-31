e62a744b47bcde6320d0a7a1553a6c720ba50325
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-31 15:52:56 +0200

    Added access control response headers

--- config/middleware.php
+++ config/middleware.php
@@ -3,3 +3,12 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
+$app->add(function (Request $request, Response $response, callable $next) {
+    /* @var $response Response */
+    $response = $next($request, $response);
+
+    return $response
+        ->withAddedHeader('Access-Control-Allow-Origin', '*')
+        ->withAddedHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withAddedHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
