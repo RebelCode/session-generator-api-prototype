11a6c65d65ff3b9790c6c677fb212491990f6866
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-22 14:00:02 +0200

    Added a simple checkout page mock file

---
+++ public/checkout.php
@@ -0,0 +1,16 @@
+
+<form method="POST" action="">
+    <label>
+        First Name:
+        <input type="text" name="customer-name"/>
+    </label>
+    <label>
+        Last Name:
+        <input type="text" name="customer-surname"/>
+    </label>
+    <label>
+        Email Address:
+        <input type="email" name="customer-email"/>
+    </label>
+    <input type="submit" />
+</form>
